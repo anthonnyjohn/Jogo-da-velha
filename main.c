6b6befdff2d5e8199b8879c5ee9051341dca0590
AryssonLuz <arysson2020@gmail.com>
2023-11-01 11:40:34 -0300

    Primeira mudança no jogo da velha
    esse main foi modificado até os 24:42 minutos do video:https://www.youtube.com/watch?v=WPVrj4CyCvM&ab_channel=Programeseufuturo

--- main.c
+++ main.c
@@ -5,7 +5,52 @@
 
 int main(int argc, char *argv[]) {
 	
+	int i,j,linha,coluna = 0;
+	char tabu[3][3];
 	
+	
+	
+	
+	for (i=0;i<3;i++){
+		
+		for(j=0;j<3;j++){
+			
+			tabu[i][j]=' ';
+						
+		}
+		
+	}
+	
+	printf("\n\n\t 0   1   2\n\n");
+	for (i=0;i<3;i++){
+		
+		for(j=0;j<3;j++){
+			if(j == 0){
+				printf("\t");
+			}
+			
+			printf(" %c",tabu[i][j]);
+			
+			if(j < 2){
+			
+				printf(" |");
+			}
+			if(j == 2){
+				
+				printf("   %d",i);
+			}
+				
+		}
+		if(i < 2){
+			printf("\n\t-----------");
+		}
+		printf("\n");
+		
+	}
+	do{
+	printf("Digite a linha e a coluna que deseja jogar:");
+	scanf("%d%d",&linha,&coluna);
+	}while(linha < 0 || linha > 2 || coluna < 0 || coluna > 2);
 	
 	
 	
